f3b69e70a305f8caae80e75dc7a473d10d388e7f
Divided by Zer0 <mail@dbzer0.com>
2021-06-21 11:22:29 +0200

    renames

---
+++ src/dreamscape/cards/sets/CardGroups.gd
@@ -0,0 +1,47 @@
+# This file contains just card definitions. See also `CardConfig.gd`
+
+extends Reference
+
+const SET = "Core Set"
+
+const CLASS := {
+	"Flyer": {
+		"Starting Cards": [
+			"Fly Upwards",
+			"Dive-in",
+			"Safety of Air",
+		],
+		"Available Cards": []
+	},
+}
+
+const RACE := {
+	"Fearless": {
+		"Starting Cards": [
+			"Assault",
+			"Assault",
+			"Laugh at Danger",
+		],
+		"Available Cards": []
+	},
+}
+
+const ITEM := {
+	"Rubber Chicken": {
+		"Starting Cards": [
+			"Confounding movements",
+			"Assault",
+			"Noisy Whip",
+		],
+		"Available Cards": []
+	},
+}
+
+const LIFE_GOAL := {
+	"Abusive Relationship": {
+		"Starting Cards": [
+			"Inner Justice",
+		],
+		"Available Cards": []
+	},
+}
--- src/dreamscape/cards/sets/SetDefinition_Core.gd
+++ src/dreamscape/cards/sets/SetDefinition_Core.gd
@@ -5,16 +5,58 @@ extends Reference
 const SET = "Core Set"
 const CARDS := {
 	"Defend": {
-		"Type": "Blue",
+		"Type": "Control",
 		"Tags": [],
 		"Abilities": "Gain 5 defense",
 		"Cost": 1,
 		"_illustration": "Nobody",
 	},
-	"Attack": {
-		"Type": "Red",
+	"Assault": {
+		"Type": "Action",
 		"Tags": [],
 		"Abilities": "Do 6 Damage to target enemy.",
+		"Cost": 1,
+		"_illustration": "Nobody",
+	},
+	"Fly Upwards": {
+		"Type": "Control",
+		"Tags": [],
+		"Abilities": "You do not take any damage from enemy intents this turn",
+		"Cost": 3,
+		"_illustration": "Nobody",
+	},
+	"Dive-in": {
+		"Type": "Control",
+		"Tags": [],
+		"Abilities": "Gain 2 Vulnerable. Your next attack does double damage.",
+		"Cost": 1,
+		"_illustration": "Nobody",
+	},
+	"Safety of Air": {
+		"Type": "Control",
+		"Tags": [],
+		"Abilities": "Reduce your anxiety by 4.",
+		"Cost": 1,
+		"_illustration": "Nobody",
+	},
+	"Laugh at Danger": {
+		"Type": "Concentration",
+		"Tags": [],
+		"Abilities": "Gain 1 Immersion at the start of each turn. Increase all anxiety taken from enemy intents by 2.",
+		"Cost": 2,
+		"_illustration": "Nobody",
+	},
+	"Confounding Movements": {
+		"Type": "Control",
+		"Tags": [],
+		"Abilities": "Gain 4 defense. Apply weaken to target enemy.",
+		"Cost": 1,
+		"_illustration": "Nobody",
+	},
+	"Noisy Whip": {
+		"Type": "Action",
+		"Tags": [],
+		"Abilities": "Do 5 Damage to target enemy and apply weaken.",
 		"Cost": 1,
 		"_illustration": "Nobody",
 	},
