11053739aad16600da8c6df1adbcce15257c2d46
Dividd By Zer0 <mail@dber0.com>
2021-07-04 22:37:41 +0200

    icons

--- src/core/Pile.gd
+++ src/core/Pile.gd
@@ -55,7 +55,7 @@ func _process(_delta) -> void:
 			obj.queue_free()
 	# We make sure to adjust our popup if cards were removed from it while it's open
 	$ViewPopup.set_as_minsize()
-	if has_cards and cfc.game_settings.focus_style:
+	if _has_cards and cfc.game_settings.focus_style:
 		var top_card = get_top_card()
 		if cfc.NMAP.board.mouse_pointer in get_overlapping_areas()\
 				and not cfc.card_drag_ongoing:
--- src/dreamscape/CombatElements/CombatEffects/Buffer.gd
+++ src/dreamscape/CombatElements/CombatEffects/Buffer.gd
@@ -1,5 +1,7 @@
 extends CombatEffect
 
+
+
 func _on_player_turn_started(turn: Turn) -> void:
 	._on_player_turn_started(turn)
 	cfc.NMAP.board.counters.mod_counter("immersion", stacks, false, false, self, ["New Turn"])
