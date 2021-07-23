51539ba30b0f758b8298949e9bc705b78cde3202
Divided by Zer0 <mail@dbzer0.com>
2021-07-23 17:46:50 +0200

    Can now set card list width properties

--- src/core/CardViewer/CardLibrary/CLListCardObject.gd
+++ src/core/CardViewer/CardLibrary/CLListCardObject.gd
@@ -1,24 +1,29 @@
 class_name CLListCardObject
 extends CVListCardObject
 
-var icon_size := '15x15'
-
 func setup(_card_name: String) -> void:
 	.setup(_card_name)
+	if card_viewer.property_width_exceptions.has(CardConfig.SCENE_PROPERTY):
+		_card_type.rect_min_size.x = card_viewer.property_width_exceptions[CardConfig.SCENE_PROPERTY]
+	else:
+		_card_type.rect_min_size.x = card_viewer.default_property_width
 	for p in card_properties:
 		var property: String = p
 		if not property.begins_with('_')\
 				and property != CardConfig.SCENE_PROPERTY:
 			var new_label := RichTextLabel.new()
 			new_label.name = property
-			new_label.rect_min_size.x = 100
+			if card_viewer.property_width_exceptions.has(property):
+				new_label.rect_min_size.x = card_viewer.property_width_exceptions[property]
+			else:
+				new_label.rect_min_size.x = card_viewer.default_property_width
 			new_label.bbcode_enabled = true
 			new_label.scroll_active = false
 			new_label.fit_content_height = true
 #			new_label.autowrap = true
 			var format = _get_bbcode_format()
 			var bbcode_format := {}
-			bbcode_format["icon_size"] = icon_size
+			bbcode_format["icon_size"] = str(card_viewer.icon_size) + 'x' + str(card_viewer.icon_size)
 			for key in format:
 				format[key] = format[key].format(bbcode_format)
 			if property in CardConfig.PROPERTIES_ARRAYS:
