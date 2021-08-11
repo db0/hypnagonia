a17eb262ce1b6bf101ac98d9cbd852140e8faf4d
Divided by Zer0 <mail@dbzer0.com>
2021-08-11 16:46:56 +0200

    Shown upgrades in card library

--- src/dreamscape/CardLibrary/ListCardObject.gd
+++ src/dreamscape/CardLibrary/ListCardObject.gd
@@ -23,3 +23,16 @@ func _get_bbcode_format() -> Dictionary:
 	for amount in amounts_format:
 		basic_bbcode_formats[amount] = amounts_format[amount]
 	return(basic_bbcode_formats)
+
+
+func _on_ListCardObject_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.get_button_index() == 1:
+		var upgrade_options = cfc.card_definitions[card_name].get("_upgrades", [])
+		var select_return = cfc.ov_utils.select_card(
+				upgrade_options, 0, "display", false, card_viewer)
+		if select_return is GDScriptFunctionState: # Still working.
+			select_return = yield(select_return, "completed")
+
+func setup_grid_card_object() -> void:
+	.setup_grid_card_object()
+	grid_card_object.connect("gui_input", self, "_on_ListCardObject_gui_input")
