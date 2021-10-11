23355165f12d785681ca511d442d81847f33ae54
Divided by Zer0 <mail@dbzer0.com>
2021-10-11 09:29:43 +0200

    fascination immersion will be captured as immersion increasing event

--- src/dreamscape/CombatElements/CombatEffects/Buffer.gd
+++ src/dreamscape/CombatElements/CombatEffects/Buffer.gd
@@ -4,6 +4,6 @@ extends CombatEffect
 
 func _on_player_turn_started(turn: Turn) -> void:
 	._on_player_turn_started(turn)
-	cfc.NMAP.board.counters.mod_counter("immersion", stacks, false, false, self, ["New Turn"])
+	cfc.NMAP.board.counters.mod_counter("immersion", stacks, false, false, self, ["Effect"])
 	set_stacks(0)
 	
