ad30f6860d236281db54ea0a3fa1b15759df4124
Divided by Zer0 <mail@dbzer0.com>
2021-12-26 23:57:39 +0100

    ref: Simplified loading of artifacts without using scenes

--- src/dreamscape/Globals/ArtifactObject.gd
+++ src/dreamscape/Globals/ArtifactObject.gd
@@ -1,6 +1,8 @@
 class_name ArtifactObject
 extends Reference
 
+const ARTIFACT_SCENE = preload("res://src/dreamscape/Artifacts/ArtifactTemplate.tscn")
+
 signal removed
 signal counter_modified(counter_value)
 
@@ -17,11 +19,9 @@ var context: int
 var modifiers := {}
 
 func _init(artifact_name: String, _mods := {}) -> void:
-	artifact_scene = load(
-			"res://src/dreamscape/Artifacts/{artifact_name}.tscn".format(
-			{"artifact_name":artifact_name}))
+	artifact_scene = ARTIFACT_SCENE
 	definition = ArtifactDefinitions[artifact_name].duplicate(true)
-	canonical_name = definition["name"]
+	canonical_name = definition["canonical_name"]
 	context = definition["context"]
 	modifiers = _mods
 
@@ -31,3 +31,12 @@ func remove_self() -> void:
 func set_counter(value: int) -> void:
 	counter = value
 	emit_signal("counter_modified", counter)
+
+func instance_artifact() -> Artifact:
+	var artifact: Artifact = artifact_scene.instance()
+	var script_path := "res://src/dreamscape/Artifacts/%s.gd" % [canonical_name]
+	var script_exists = Directory.new()
+	if script_exists.file_exists(script_path):
+		var artifact_script = load(script_path)
+		artifact.set_script(artifact_script)
+	return(artifact)
