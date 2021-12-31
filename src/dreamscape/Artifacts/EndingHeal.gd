bdcb43c3574ac7ee64670d56c037d6d950b9c1d9
Divided by Zer0 <mail@dbzer0.com>
2021-12-31 13:29:14 +0100

    fix(curios): Won't crash on some curios
    Fixed crash on ImproveFortify and ImproveImpervious
    Fixes logic issue on EndingHeal
    Closes #114

--- src/dreamscape/Artifacts/EndingHeal.gd
+++ src/dreamscape/Artifacts/EndingHeal.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	if is_active and effect_context == ArtifactDefinitions.EffectContext.BATTLE:
 		if not cfc.are_all_nodes_mapped:
 			yield(cfc, "all_nodes_mapped")
-			cfc.NMAP.board.connect("battle_ended", self, "_on_battle_ended")
+		cfc.NMAP.board.connect("battle_ended", self, "_on_battle_ended")
 
 func _on_battle_ended() -> void:
 	globals.player.damage -= 6
