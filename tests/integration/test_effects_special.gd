8d09da6f3417ad19558738bea11af3441e2242dd
Divided by Zer0 <mail@dbzer0.com>
2022-01-13 00:26:22 +0100

    tests: Added first Surreality Tests

--- tests/integration/test_effects_special.gd
+++ tests/integration/test_effects_special.gd
@@ -110,33 +110,3 @@ class TestJumbletron:
 				assert_signal_emitted(ce, "card_entry_modified")
 			else:
 				assert_signal_not_emitted(ce, "card_entry_modified")
-
-# Need to move this to a new file
-#class TestSurrealBoss:
-#	extends "res://tests/HUT_TormentEffectsTestClass.gd"
-#
-#	var effect = Terms.ACTIVE_EFFECTS.surreal_boss.name
-#	func _init() -> void:
-#		test_card_names = [
-#			"Interpretation",
-#			"Confidence",
-#		]
-#		effects_to_play = [
-#			{
-#				"name": effect,
-#				"amount": 1,
-#			}
-#		]
-#
-#
-#	func test_surreal_boss():
-#		cards[0].scripts = BIG_ATTACK_SCRIPT
-#		cards[1].scripts = BIG_DEFENCE_SCRIPT
-#		cards[1].execute_scripts()
-#		var sceng = snipexecute(cards[0], test_torment)
-#		if sceng is GDScriptFunctionState:
-#			sceng = yield(sceng, "completed")
-#		assert_eq(test_torment.active_effects.get_effect_stacks(effect), 1,
-#				"%s gave %s due to massive damage" % [effect, Terms.ACTIVE_EFFECTS.strengthen.name])
-#		cfc.NMAP.board.turn.end_player_turn()
-#		yield(yield_to(board.turn, "player_turn_started",3 ), YIELD)
