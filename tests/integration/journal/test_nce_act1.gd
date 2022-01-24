2b0aea81890a3f05cf31bfe86bd03d3929e43a1a
Divided by Zer0 <mail@dbzer0.com>
2022-01-24 12:03:38 +0100

    tests: Added act2 nce

--- tests/integration/journal/test_nce_act1.gd
+++ tests/integration/journal/test_nce_act1.gd
@@ -1,42 +1,5 @@
 extends "res://tests/HUT_Journal_NCETestClass.gd"
 
-class TestTheCandyman:
-	extends  "res://tests/HUT_Journal_NCETestClass.gd"
-	func _init() -> void:
-		testing_nce_script = preload("res://src/dreamscape/Run/NCE/AllActs/TheCandyman.gd")
-
-	func test_selecting_with_starting_deck():
-		cfc.game_rng_seed = CFUtils.generate_random_seed()
-		gut.p("Testing Random Seed: " + cfc.game_rng_seed)
-		begin_nce_with_choices(nce)
-		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
-		activate_secondary_choice_by_index(0)
-		yield(yield_to(journal, "selection_deck_spawned", 0.2), YIELD)
-		assert_signal_emitted(journal, "selection_deck_spawned")
-		var selection_decks =  cfc.get_tree().get_nodes_in_group("selection_decks")
-		assert_eq(selection_decks.size(), 1, "Selected Deck spawned")
-		if selection_decks.size() == 0:
-			return
-		var selection_deck : SelectionDeck = selection_decks[0]
-		watch_signals(globals.player.deck)
-		selection_deck._deck_preview_grid.get_children()[0].select_card()
-		assert_signal_emitted(globals.player.deck, "card_removed")
-		assert_signal_emitted(globals.player.deck, "card_added")
-		var signal_details = get_signal_parameters(nested_choices_scene, "secondary_choice_selected")
-		assert_gt(signal_details.size(), 0, "secondary option selected")
-		if signal_details.size() >= 1:
-			var choice_key = signal_details[0]
-			var removed_card = get_signal_parameters(globals.player.deck, "card_removed")
-			var added_card = get_signal_parameters(globals.player.deck, "card_added")
-			if removed_card.size() > 0 and added_card.size() > 0:
-				var cards = [removed_card[0], added_card[0]]
-				var used_indexes := []
-				for index in range(2):
-					for c in cards:
-						if c.properties.Type == HConst.COLOUR_MAP[choice_key[index]] and not index in used_indexes:
-							used_indexes.append(index)
-				assert_eq(used_indexes.size(), 2, "Added and Removed cards match the candy colours")
-
 class TestDollmaker:
 	extends  "res://tests/HUT_Journal_NCETestClass.gd"
 	func _init() -> void:
@@ -102,7 +65,6 @@ class TestMonsterTrain:
 		activate_secondary_choice_by_key("lead")
 		yield(yield_to(globals.player, "artifact_added", 0.2), YIELD)
 		assert_signal_emitted(globals.player, "artifact_added")
-		assert_eq(globals.player.damage, 15, "Player took damage")
 
 	func test_choice_lead_failure():
 		cfc.game_rng_seed = "<f<s_=ZJp@"
@@ -256,64 +218,6 @@ class TestPopPsychologist1:
 		assert_pathos_signaled("released_pathos_gained", Terms.RUN_ACCUMULATION_NAMES.shop)
 		assert_nce_unlocked(preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist2.gd"))
 
-class TestPopPsychologist2:
-	extends  "res://tests/HUT_Journal_NCETestClass.gd"
-	func _init() -> void:
-		testing_nce_script = preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist2.gd")
-
-	func test_choice_passion_fruit():
-		watch_signals(globals.encounters.run_changes)
-		begin_nce_with_choices(nce)
-		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
-		watch_signals(globals.player.pathos)
-		activate_secondary_choice_by_key("passion fruit")
-		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
-		assert_pathos_signaled("released_pathos_gained", Terms.RUN_ACCUMULATION_NAMES.artifact)
-		assert_nce_unlocked(preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist3.gd"))
-	func test_choice_orange():
-		watch_signals(globals.encounters.run_changes)
-		begin_nce_with_choices(nce)
-		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
-		watch_signals(globals.player.pathos)
-		activate_secondary_choice_by_key("orange")
-		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
-		assert_pathos_signaled("released_pathos_gained", Terms.RUN_ACCUMULATION_NAMES.elite)
-		assert_nce_unlocked(preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist3.gd"))
-	func test_choice_bananal():
-		watch_signals(globals.encounters.run_changes)
-		begin_nce_with_choices(nce)
-		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
-		watch_signals(globals.player.pathos)
-		activate_secondary_choice_by_key("banana")
-		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
-		assert_pathos_signaled("released_pathos_gained", Terms.RUN_ACCUMULATION_NAMES.rest)
-		assert_nce_unlocked(preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist3.gd"))
-
-class TestPopPsychologist3:
-	extends  "res://tests/HUT_Journal_NCETestClass.gd"
-	func _init() -> void:
-		testing_nce_script = preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist3.gd")
-		dreamer_starting_damage = 70
-
-	func test_choice_death():
-		begin_nce_with_choices(nce)
-		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
-		activate_secondary_choice_by_key("death")
-		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
-		assert_eq(globals.player.damage, 0)
-	func test_choice_release():
-		begin_nce_with_choices(nce)
-		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
-		activate_secondary_choice_by_key("release")
-		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
-		assert_eq(globals.player.damage, 0)
-	func test_choice_oblivion():
-		begin_nce_with_choices(nce)
-		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
-		activate_secondary_choice_by_key("oblivion")
-		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
-		assert_eq(globals.player.damage, 0)
-
 
 class TestRiskyEvent1:
 	extends  "res://tests/HUT_Journal_NCETestClass.gd"
@@ -447,4 +351,3 @@ class TestSpider:
 		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
 		assert_pathos_not_signaled("pathos_repressed")
 		assert_eq(globals.player.damage, dreamer_starting_damage + 10, "Player healed damage")
-		
---
+++ tests/integration/journal/test_nce_allacts.gd
@@ -0,0 +1,96 @@
+extends "res://tests/HUT_Journal_NCETestClass.gd"
+
+class TestTheCandyman:
+	extends  "res://tests/HUT_Journal_NCETestClass.gd"
+	func _init() -> void:
+		testing_nce_script = preload("res://src/dreamscape/Run/NCE/AllActs/TheCandyman.gd")
+
+	func test_selecting_with_starting_deck():
+		cfc.game_rng_seed = CFUtils.generate_random_seed()
+		gut.p("Testing Random Seed: " + cfc.game_rng_seed)
+		begin_nce_with_choices(nce)
+		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
+		activate_secondary_choice_by_index(0)
+		yield(yield_to(journal, "selection_deck_spawned", 0.2), YIELD)
+		assert_signal_emitted(journal, "selection_deck_spawned")
+		var selection_decks =  cfc.get_tree().get_nodes_in_group("selection_decks")
+		assert_eq(selection_decks.size(), 1, "Selected Deck spawned")
+		if selection_decks.size() == 0:
+			return
+		var selection_deck : SelectionDeck = selection_decks[0]
+		watch_signals(globals.player.deck)
+		selection_deck._deck_preview_grid.get_children()[0].select_card()
+		assert_signal_emitted(globals.player.deck, "card_removed")
+		assert_signal_emitted(globals.player.deck, "card_added")
+		var signal_details = get_signal_parameters(nested_choices_scene, "secondary_choice_selected")
+		assert_gt(signal_details.size(), 0, "secondary option selected")
+		if signal_details.size() >= 1:
+			var choice_key = signal_details[0]
+			var removed_card = get_signal_parameters(globals.player.deck, "card_removed")
+			var added_card = get_signal_parameters(globals.player.deck, "card_added")
+			if removed_card.size() > 0 and added_card.size() > 0:
+				var cards = [removed_card[0], added_card[0]]
+				var used_indexes := []
+				for index in range(2):
+					for c in cards:
+						if c.properties.Type == HConst.COLOUR_MAP[choice_key[index]] and not index in used_indexes:
+							used_indexes.append(index)
+				assert_eq(used_indexes.size(), 2, "Added and Removed cards match the candy colours")
+
+class TestPopPsychologist2:
+	extends  "res://tests/HUT_Journal_NCETestClass.gd"
+	func _init() -> void:
+		testing_nce_script = preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist2.gd")
+
+	func test_choice_passion_fruit():
+		watch_signals(globals.encounters.run_changes)
+		begin_nce_with_choices(nce)
+		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
+		watch_signals(globals.player.pathos)
+		activate_secondary_choice_by_key("passion fruit")
+		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
+		assert_pathos_signaled("released_pathos_gained", Terms.RUN_ACCUMULATION_NAMES.artifact)
+		assert_nce_unlocked(preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist3.gd"))
+	func test_choice_orange():
+		watch_signals(globals.encounters.run_changes)
+		begin_nce_with_choices(nce)
+		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
+		watch_signals(globals.player.pathos)
+		activate_secondary_choice_by_key("orange")
+		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
+		assert_pathos_signaled("released_pathos_gained", Terms.RUN_ACCUMULATION_NAMES.elite)
+		assert_nce_unlocked(preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist3.gd"))
+	func test_choice_bananal():
+		watch_signals(globals.encounters.run_changes)
+		begin_nce_with_choices(nce)
+		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
+		watch_signals(globals.player.pathos)
+		activate_secondary_choice_by_key("banana")
+		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
+		assert_pathos_signaled("released_pathos_gained", Terms.RUN_ACCUMULATION_NAMES.rest)
+		assert_nce_unlocked(preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist3.gd"))
+
+class TestPopPsychologist3:
+	extends  "res://tests/HUT_Journal_NCETestClass.gd"
+	func _init() -> void:
+		testing_nce_script = preload("res://src/dreamscape/Run/NCE/AllActs/PopPsychologist3.gd")
+		dreamer_starting_damage = 70
+
+	func test_choice_death():
+		begin_nce_with_choices(nce)
+		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
+		activate_secondary_choice_by_key("death")
+		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
+		assert_eq(globals.player.damage, 0)
+	func test_choice_release():
+		begin_nce_with_choices(nce)
+		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
+		activate_secondary_choice_by_key("release")
+		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
+		assert_eq(globals.player.damage, 0)
+	func test_choice_oblivion():
+		begin_nce_with_choices(nce)
+		yield(yield_to(journal, "secondary_entry_added", 0.2), YIELD)
+		activate_secondary_choice_by_key("oblivion")
+		yield(yield_to(nce, "encounter_end", 0.2), YIELD)
+		assert_eq(globals.player.damage, 0)
