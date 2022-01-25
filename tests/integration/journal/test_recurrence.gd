ea4b9d1dfcd037b7b3e55e871d8d6107438a69b0
Divided by Zer0 <mail@dbzer0.com>
2022-01-25 15:28:37 +0100

    tests: hopefully fix for script error

--- tests/integration/journal/test_recurrence.gd
+++ tests/integration/journal/test_recurrence.gd
@@ -9,8 +9,28 @@ class TestNCE:
 		begin_surprise_encounter(nce)
 		yield(yield_to(cfc, "all_nodes_mapped", 3), YIELD)
 		assert_has(cfc.NMAP, "board")
+		if not cfc.NMAP.has("board"):
+			return
+		yield(yield_to(cfc.NMAP.board, "battle_begun", 2), YIELD)
 		watch_signals(globals.encounters.run_changes)
 		watch_signals(globals.player)
+		var recurrence
+		if get_tree().get_nodes_in_group("EnemyEntities").size() > 0:
+			recurrence = get_tree().get_nodes_in_group("EnemyEntities")[0]
+		assert_not_null(recurrence, "Recurrence spawned")
+		if not recurrence:
+			return
+		assert_connected(cfc.NMAP.board.dreamer, recurrence,
+				"effect_modified", "_on_dreamer_effect_modified")
+		assert_connected(cfc.NMAP.board.dreamer, recurrence,
+				"entity_defended", "_on_dreamer_defended")
+		assert_connected(cfc.NMAP.board.dreamer, recurrence,
+				"entity_healed", "_on_dreamer_healed")
+		assert_connected(cfc.NMAP.board.dreamer, recurrence,
+				"entity_damaged", "_on_dreamer_damaged")
+		assert_connected(cfc.signal_propagator, recurrence,
+				"signal_received", "_on_card_signal_received")
+		assert_eq(recurrence.get_property("_difficulty"), "easy", "Difficulty set correctly")
 		end_surprise_encounter()
 		assert_nce_unlocked(preload("res://src/dreamscape/Run/NCE/AllActs/Recurrence.gd"))
 		assert_signal_emit_count(globals.player, "memory_added", 2)
@@ -25,14 +45,17 @@ class TestNCE:
 		watch_signals(mem3)
 		watch_signals(mem4)
 		begin_surprise_encounter(nce)
-		yield(yield_to(cfc, "all_nodes_mapped", 3), YIELD)
+		yield(yield_to(cfc, "all_nodes_mapped", 3), YIELD)		
 		assert_has(cfc.NMAP, "board")
+		if not cfc.NMAP.has("board"):
+			return
+		yield(yield_to(cfc.NMAP.board, "battle_begun", 2), YIELD)
 		watch_signals(globals.encounters.run_changes)
 		watch_signals(globals.player)
 		end_surprise_encounter()
 		assert_nce_unlocked(preload("res://src/dreamscape/Run/NCE/AllActs/Recurrence.gd"))
 		assert_signal_emit_count(globals.player, "memory_added", 1)
-		assert_signal_emitted_with_parameters(mem3, "memory_upgraded", [mem3,2])
-		assert_signal_not_emitted(mem1, "memory_upgraded")
+		assert_signal_emitted_with_parameters(mem1, "memory_upgraded", [mem1,2])
 		assert_signal_not_emitted(mem2, "memory_upgraded")
+		assert_signal_not_emitted(mem3, "memory_upgraded")
 		assert_signal_not_emitted(mem4, "memory_upgraded")
