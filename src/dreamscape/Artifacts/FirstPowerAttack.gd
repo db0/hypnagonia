456e9f31683949fd92af00c8ce176a5ea853e341
Divided by Zer0 <mail@dbzer0.com>
2022-03-27 00:00:59 +0100

    fix(curios): Rosetta Stone
    Rosetta Stone now correctly affects repeat predictions
    
    Artifacts can now store if they're still active during
    sceng snapshot runs when they're alterants

--- src/dreamscape/Artifacts/FirstPowerAttack.gd
+++ src/dreamscape/Artifacts/FirstPowerAttack.gd
@@ -3,19 +3,22 @@ extends Artifact
 func get_effect_alteration(
 		script: ScriptTask, 
 		value: int, 
-		_sceng, 
+		sceng, 
 		is_source := false, 
 		_dry_run := true,
 		_subject: Node = null) -> int:
+	var is_currently_active = snapshot_is_active.get(sceng.snapshot_id, is_active)
 	if not script.script_name == 'modify_damage'\
 			or not "Attack" in script.get_property(SP.KEY_TAGS)\
 			or not is_source\
-			or not is_active:
+			or not is_currently_active:
 		return(0)
 	var new_value = value + ArtifactDefinitions.FirstPowerAttack.amounts.effect_amount
 	var alteration = new_value - value
 	# This effect triggers only once per battle
-	if not _dry_run:
+	if sceng.snapshot_id > 0:
+		snapshot_is_active[sceng.snapshot_id] = false
+	elif not _dry_run:
 		is_active = false
 		_send_trigger_signal()
 	return(alteration)
