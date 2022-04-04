49f70db3e8c47291314b8dff858ba84e8503c5bc
Divided by Zer0 <mail@dbzer0.com>
2022-04-04 11:07:56 +0200

    feat(journal): Archetype-specific victory texts
    some Initial texts by @SkylarkHYP

--- src/dreamscape/Overworld/JournalTexts.gd
+++ src/dreamscape/Overworld/JournalTexts.gd
@@ -27,3 +27,13 @@ const ACT_CHANGE_TEXTS := [
 	"This felt like a conclusion, at which point I had a period of deep sleep. "\
 			+ "But my dreams were not over yet; the worst was yet to come..."
 ]
+
+const VICTORY_TEXTS := {
+	"generic": [
+		"After all of this, every single thing I've had to endure through this decrepit and endless spiral, a blinding warmth washes over me. Feeling returns as I lay on something soft. What is soft, what is comfort? It''s been hours to days, weeks, months to years! I've forgotten what it feels like to be calm, no nightmares, no confusion, no more anxiety. I repeat that to myself in my head. No more anxiety... No more anxiety. No more! It's time, it's time for me to wake up and make a change. I open my eyes slowly, and the morning sun slips into view, I feel pensive as I ponder all the ways I can interpret the problems in my life, and find the confidence to overcome them.",
+	],
+	"Abusive Relationship": [
+		"I know what it like to think for myself, I am confident and courageous. I won't tolerate mistreatment from anyone anymore. I will cast away their hurtful words and actions, and sever dependence from them. I will be beyond their torment and stress. I hope to still find love one day, but this time clarity will keep me from doubting myself. I've learned to stand up for myself, and continue to persevere. I've truly found my inner justice.",
+		"I woke up with something changed in me. I could feel my partner's hold on me slipping. My friends were always pointing out what should have been clear to me for a long time and this time I will listen. It is time to stop being a victim."
+	]
+}
