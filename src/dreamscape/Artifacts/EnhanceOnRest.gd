61714d1ffef0a2a37d9c72714509e11c19c6f86d
Divided by Zer0 <mail@dbzer0.com>
2022-04-05 12:21:50 +0200

    content(curio): EnhanceOnRest

---
+++ src/dreamscape/Artifacts/EnhanceOnRest.gd
@@ -0,0 +1,7 @@
+extends Artifact
+
+func setup(signifier_details: Dictionary, signifier_name: String):
+	.setup(signifier_details,signifier_name)
+	update_amount(artifact_object.counter)
+	if artifact_object.counter >= ArtifactDefinitions.EnhanceOnRest.max_uses:
+		_activate()
--- src/dreamscape/Run/NCE/Rest.gd
+++ src/dreamscape/Run/NCE/Rest.gd
@@ -13,7 +13,8 @@ var secondary_choices := {
 
 var unlockable_choices := {
 	"strengthen_up": '[%s] Increase starting {strengthen} by 1.' % [ArtifactDefinitions.StrengthenUp.name],
-	"quicken_up": '[%s] Increase starting {strengthen} by 1.' % [ArtifactDefinitions.QuickenUp.name]
+	"quicken_up": '[%s] Increase starting {strengthen} by 1.' % [ArtifactDefinitions.QuickenUp.name],
+	"enhance": '[%s] Enhance one card.' % [ArtifactDefinitions.EnhanceOnRest.name],
 }
 
 var reward_texts := {
@@ -23,6 +24,7 @@ var reward_texts := {
 	"resist": '...and I struggled mightily to stave it off.',
 	"strengthen_up": 'I remembered I was carrying around this curious Buddha. I deciced to give meditation a try.',
 	"quicken_up": 'I roused myself just enough to play a quick game of chess with myself. It made me feel more grounded.',
+	"enhance": 'The Dream Catcher stirred and displayed what came before. I could use this knowledge to improve the way I act from now on.',
 	
 }
 
@@ -51,6 +53,11 @@ func begin() -> void:
 		secondary_choices['quicken_up'] = unlockable_choices['quicken_up']
 		if quicken_up.counter >= ArtifactDefinitions.StrengthenUp.max_uses:
 			disabled_choices.append('quicken_up')
+	var enhance = globals.player.find_artifact(ArtifactDefinitions.EnhanceOnRest.canonical_name)
+	if enhance:
+		secondary_choices['enhance'] = unlockable_choices['enhance']
+		if enhance.counter >= ArtifactDefinitions.EnhanceOnRest.max_uses:
+			disabled_choices.append('enhance')
 	if globals.player.deck.count_progressing_cards() < 1:
 		disabled_choices.append('progress')
 	_prepare_secondary_choices(secondary_choices, scformat, disabled_choices)
@@ -74,6 +81,20 @@ func continue_encounter(key) -> void:
 		"quicken_up":
 			var quicken_up = globals.player.find_artifact(ArtifactDefinitions.QuickenUp.canonical_name)
 			quicken_up.counter += 1
+		"enhance":
+			var selection_deck : SelectionDeck = globals.journal.spawn_selection_deck()
+			selection_deck.popup_exclusive = true
+			# warning-ignore:return_value_discarded
+			var card_filters = [CardFilter.new('Type', 'Concentration', 'ne')]
+			selection_deck.card_filters = card_filters
+			selection_deck.connect("operation_performed", self, "_on_card_selected", [key])
+			selection_deck.auto_close = true
+			selection_deck.initiate_card_selection()
+			var select_blurb := "(Enhance Card)"
+			selection_deck.update_header(select_blurb)
+			selection_deck.update_color(Color(0,1,0))
+			var enhance = globals.player.find_artifact(ArtifactDefinitions.EnhanceOnRest.canonical_name)
+			enhance.counter += 1
 	globals.journal.display_nce_rewards(reward_texts[key])
 	end()
 
@@ -82,3 +103,5 @@ func _on_card_selected(operation_details: Dictionary, key: String) -> void:
 	var chosen_card: CardEntry = operation_details.card_entry
 	if key == "progress":
 		chosen_card.upgrade_progress += progress_amount
+	if key == "enhance":
+		chosen_card.enhance()
