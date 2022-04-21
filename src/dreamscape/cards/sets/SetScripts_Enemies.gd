1d58b45f6a41fc5a8032f1062cbc8aa8a12e7ede
Divided by Zer0 <mail@dbzer0.com>
2022-04-21 09:53:46 +0200

    fix(cards): Improved IC

--- src/dreamscape/cards/sets/SetScripts_Enemies.gd
+++ src/dreamscape/cards/sets/SetScripts_Enemies.gd
@@ -983,6 +983,12 @@ const scripts := {
 						"value_key": "defence_amount"
 					},
 				},
+				{
+					"name": "move_card_to_container",
+					"subject": "self",
+					"dest_container": "forgotten",
+					"tags": ["Played", "Card"],
+				},
 			],
 		},
 	},
