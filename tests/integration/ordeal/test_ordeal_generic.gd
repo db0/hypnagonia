618d77bec4791f95dc8db41132d97ddae08e9851
Divided by Zer0 <mail@dbzer0.com>
2022-04-30 14:48:43 +0200

    tests: Reorg

--- tests/integration/ordeal/test_ordeal_generic.gd
+++ tests/integration/ordeal/test_ordeal_generic.gd
@@ -100,32 +100,6 @@ class TestRounds:
 			assert_signal_emit_count(t, "finished_activation", 1)	
 
 
-class TestTags:
-	extends "res://tests/HUT_Ordeal_GenericTestClass.gd"
-
-	var alpha1: CardEntry
-	var alpha2: CardEntry
-	var omega1: CardEntry
-
-	func _init() -> void:
-		globals.test_flags["no_refill"] = false
-		globals.test_flags["test_initial_hand"] = true
-
-	func extra_hypnagonia_setup() -> void:
-		alpha1 = globals.player.deck.add_new_card("^ Sneaky-Beaky ^")
-		alpha2 = globals.player.deck.add_new_card("^ Absurdity Unleashed ^")
-		omega1 = globals.player.deck.add_new_card("Ω Excogitate Ω")
-
-	func test_frozen_alpha_and_omega():
-		alpha1.card_object.properties.Tags.append(Terms.GENERIC_TAGS.frozen.name)
-		assert_true(hand.has_card(alpha1.card_object), "Alpha cards should start in hand")
-		assert_true(hand.has_card(alpha2.card_object), "Alpha cards should start in hand")
-		assert_eq(deck.get_bottom_card(), omega1.card_object, "Omega cards should start at the deck bottom")
-		turn.call_deferred("end_player_turn")
-		yield(yield_to(turn, "player_turn_started", 3), YIELD)
-		assert_true(hand.has_card(alpha1.card_object), "Frozen cards should stay in hand")
-		assert_eq(hand.get_card_count(), 6, "Hand refill ignores frozen cards")
-
 class TestTurnEventRecording:
 	extends "res://tests/HUT_Ordeal_GenericTestClass.gd"
 
