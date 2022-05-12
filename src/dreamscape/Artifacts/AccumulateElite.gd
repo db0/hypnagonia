a93d7c371e14d3b7febd266b38725e6a5279d61b
Divided by Zer0 <mail@dbzer0.com>
2022-05-12 19:40:31 +0200

    balance(curios): Buffed some accumulation curios

--- src/dreamscape/Artifacts/AccumulateElite.gd
+++ src/dreamscape/Artifacts/AccumulateElite.gd
@@ -4,4 +4,5 @@ func _on_artifact_added() -> void:
 	globals.player.pathos.repress_pathos(
 			Terms.RUN_ACCUMULATION_NAMES.elite,
 			ArtifactDefinitions.AccumulateElite.amounts.pathos_amount)
+	globals.player.health += ArtifactDefinitions.AccumulateElite.amounts.anxiety_amount
 	_send_trigger_signal()
--- src/dreamscape/Artifacts/AccumulateEnemy.gd
+++ src/dreamscape/Artifacts/AccumulateEnemy.gd
@@ -4,4 +4,5 @@ func _on_artifact_added() -> void:
 	globals.player.pathos.repress_pathos(
 			Terms.RUN_ACCUMULATION_NAMES.enemy,
 			ArtifactDefinitions.AccumulateEnemy.amounts.pathos_amount)
+	globals.player.damage -= ArtifactDefinitions.AccumulateEnemy.amounts.relax_amount
 	_send_trigger_signal()
