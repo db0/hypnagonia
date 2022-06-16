2d6966ee9375d8d097a674b52cc681b62836178b
Divided by Zer0 <mail@dbzer0.com>
2022-06-16 22:48:38 +0200

    tests: more failsafes

--- tests/integration/ordeal/test_ordeal_end_artifacts.gd
+++ tests/integration/ordeal/test_ordeal_end_artifacts.gd
@@ -13,5 +13,7 @@ class TestEndingHeal:
 		if not assert_has_amounts():
 			return
 		board.call_deferred("complete_battle")
+		# Because the artifact might have despawned by the time we check it
+		var heal_amount = get_amount("heal_amount")
 		yield(yield_to(EventBus, "battle_ended", 0.2), YIELD)
-		assert_eq(globals.player.damage, dreamer_starting_damage - get_amount("heal_amount"))
+		assert_eq(globals.player.damage, dreamer_starting_damage - heal_amount)
